0233d03ad38336d93a9e7bbb1797577d599dd949
Kleer LA <kleer@kleer-sandbox.(none)>
2016-09-01 15:01:41 -0300

    Ver Numero & Ver Links

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 
 get '/' do
-    
+	@secuencia = 1
+	@resultado = "Ganaste"
+    erb :tablero
 end
---
+++ features/step_definitions/my_steps.rb
@@ -0,0 +1,17 @@
+Given(/^voy al sitio$/) do
+  visit '/'
+end
+
+Then(/^veo "(.*?)"$/) do |text|
+	last_response.body.should =~ /#{text}/m
+end
+
+Given(/^secuencia es (\d+)$/) do |numero|
+	secuencia = "Secuencia: "+numero
+	last_response.body.should =~ /#{secuencia}/m
+end
+
+When(/^click (\d+)$/) do |numero|
+	click_link(numero)
+end
+
